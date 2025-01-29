562a545de2969ca7fe32656c2fa8e45af3597a0f
Catalin Secosan <secosan.catalinluca@gmail.com>
2025-01-30 00:56:57 +0200

    Added exception middleware

--- api/Univent/Univent.Api/Program.cs
+++ api/Univent/Univent.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Univent.Api.Extensions;
+using Univent.Api.Middlewares;
 using Univent.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -28,6 +29,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseAuthorization();
 

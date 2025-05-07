e1e7406ceebacb7962398d2fb000a54ef678fda3
Catalin-Luca Secosan <secosan.catalinluca@gmail.com>
2025-05-07 19:41:51 +0300

    Implemented functional not found and forbidden pages

--- client/Univent/src/app/features/errors/forbidden/forbidden.component.ts
+++ client/Univent/src/app/features/errors/forbidden/forbidden.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterModule } from '@angular/router';
+import { TokenService } from '../../../core/services/token.service';
 
 @Component({
   selector: 'app-forbidden',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    RouterModule
+  ],
   templateUrl: './forbidden.component.html',
   styleUrl: './forbidden.component.scss'
 })
 export class ForbiddenComponent {
-
+  private router = inject(Router);
+    private tokenService = inject(TokenService);
+  
+    handleRedirect(): void {
+      const token = localStorage.getItem('uniapi-token');
+  
+      if (!token) {
+        this.router.navigateByUrl('/');
+        return;
+      }
+  
+      const role = this.tokenService.getUserRole(token);
+  
+      switch (role) {
+        // Admin
+        case 0:
+          this.router.navigateByUrl('/admin/dashboard');
+          break;
+        // Student
+        case 1:
+          this.router.navigateByUrl('/home');
+          break;
+        default:
+          this.router.navigateByUrl('/');
+      }
+    }
 }
--- client/Univent/src/app/features/errors/not-found/not-found.component.ts
+++ client/Univent/src/app/features/errors/not-found/not-found.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterModule } from '@angular/router';
+import { TokenService } from '../../../core/services/token.service';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    RouterModule
+  ],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
 export class NotFoundComponent {
+  private router = inject(Router);
+  private tokenService = inject(TokenService);
 
+  handleRedirect(): void {
+    const token = localStorage.getItem('uniapi-token');
+
+    if (!token) {
+      this.router.navigateByUrl('/');
+      return;
+    }
+
+    const role = this.tokenService.getUserRole(token);
+
+    switch (role) {
+      // Admin
+      case 0:
+        this.router.navigateByUrl('/admin/dashboard');
+        break;
+      // Student
+      case 1:
+        this.router.navigateByUrl('/home');
+        break;
+      default:
+        this.router.navigateByUrl('/');
+    }
+  }
 }
